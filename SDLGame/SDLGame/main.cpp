d4122d70de938daf8e13813796e5b4a9a53e82f6
20171278 <hyeyoon156@gmail.com>
2018-11-15 23:41:19 +0900

    PP02.BreakUpHelloSDL

--- SDLGame/SDLGame/main.cpp
+++ SDLGame/SDLGame/main.cpp
@@ -1,29 +1,48 @@
 #include <SDL.h>
-
 SDL_Window* g_pWindow = 0;
 SDL_Renderer* g_pRenderer = 0;
 
-int main(int argc, char* args[])
+bool g_bRunning = false;
+
+bool init(const char* title, int xpos, int ypos, int width, int height, int flags)
 {
 	if (SDL_Init(SDL_INIT_EVERYTHING) >= 0)
 	{
-		g_pWindow = SDL_CreateWindow("PP01.HelloSDL", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN);
-
+		g_pWindow = SDL_CreateWindow(title, xpos, ypos, width, height, flags);
 		if (g_pWindow != 0)
 		{
 			g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
 		}
+	}
+	else
+	{
+		return false;
+	}
+	return true;
+}
 
+void render()
+{
+	SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 0, 255);
+	SDL_RenderClear(g_pRenderer);
+	SDL_RenderPresent(g_pRenderer);
+}
+
+
+int main(int argc, char* args[])
+{
+	if (init("Chapter 1 : Setting up SDL", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN))
+	{
+		g_bRunning = true;
 	}
 	else
 	{
 		return 1; // sdl could not initialize
 	}
-
-	SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 0, 255);
-	SDL_RenderClear(g_pRenderer);      // 윈도우를 지정된 색으로 지움 
-	SDL_RenderPresent(g_pRenderer);   // 윈도우 보이기
-	SDL_Delay(5000);                          // 5초 동안 지연
+	while (g_bRunning)
+	{
+		render();
+	}
 	SDL_Quit();                                   //  SDL 정리 
 	return 0;
 }
