2cdc80b436727a28f838ae255f9901002bb9bf0b
20171278 <hyeyoon156@gmail.com>
2018-11-16 04:02:58 +0900

    PP09.Inheritance

--- SDLGame/SDLGame/Game.h
+++ SDLGame/SDLGame/Game.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "SDL.h"
 #include "TextureManager.h"
+#include "Player.h"
 
 class Game
 {
@@ -21,4 +22,8 @@ private:
 
 	int m_currentFrame;
 	TextureManager m_textureManager;
+
+	GameObject m_go;
+	Player m_player;
+
 };
